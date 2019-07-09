5387a6fcc6e97a7d5770b9325e7a3290ce5f3b5e
Keve Cohen <klcohen@iu.edu>
2019-07-08 20:37:36 -0400

    dropdown appears

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-
+  import { Component } from '@angular/core';
+  
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent{
+
+  
 
   constructor() { }
 
